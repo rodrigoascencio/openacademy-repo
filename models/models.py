14a179b7ed149b6f7552a7185cd0bd9b6e2ce2a1
rodrigoascencio <rodrigo@vauxoo.com>
2021-02-07 15:05:44 -0600

    [ADD] campos y valores calculados

--- models/models.py
+++ models/models.py
@@ -23,9 +23,10 @@ class Session(models.Model):
     start_date = fields.Date()
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
     instructor_id = fields.Many2one(
         'res.partner', string="Instructor", domain=[
-            ('instructor', '"', True)])
+            '|',('instructor', '"', True)])
 
     instructor_id = fields.Many2one('res.partner', string="Instructor")
     course_id = fields.Many2one(
@@ -34,3 +35,12 @@ class Session(models.Model):
         string="Course",
         required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+
+    @api.depends('seats', 'attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.take_seats = 0.0
+            else:
+                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
