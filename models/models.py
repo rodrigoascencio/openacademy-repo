6e60c0911107ee74682a7cb6ea19c1c3702b0680
rodrigoascencio <rodrigo@vauxoo.com>
2021-02-07 19:18:23 -0600

    [ADD] seguridad

--- models/models.py
+++ models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
+from datetime import timedelta
 
 
 class Course(models.Model):
@@ -36,6 +37,11 @@ class Session(models.Model):
         required=True)
     attendee_ids = fields.Many2many('res.partner', string="Attendees")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+    end_date = fields.Date(
+        string="End Date",
+        store=True,
+        compute="_get_end_date",
+        inverse='_set_end_date')
 
     @api.multi
     def copy(self, default=None):
@@ -51,13 +57,24 @@ class Session(models.Model):
         default['name'] = new_name
         return super(Course, self).copy(default)
 
-    @api.depends('seats', 'attendee_ids')
+    @api.depends('start_date', 'duration')
     def _taken_seats(self):
         for r in self:
-            if not r.seats:
-                r.take_seats = 0.0
-            else:
-                r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+            if not (r.start_date and r.duration):
+                r.end_date = r.start_date
+                continue
+            start = fields.Datetime.from_string(r.start_date)
+            duration = timedelta(days=r.duration, seconds=-1)
+            r.end_date = start + duration
+
+    def _set_end_date(self):
+        for r in self:
+            if not(r.start_date and r.end_date):
+                continue
+
+            start_date = fields.Datetime.from_string(r.start_date)
+            end_date = fields.Datetime.from_string(r.end_date)
+            r.duration = (end_date - start_date).days + 1
 
     @api.onchange('seats', 'attendee_ids')
     def _verify_valid_seats(self):
